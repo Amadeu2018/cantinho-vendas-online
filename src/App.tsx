0104af4fc4d961ed8fce1a1d0193d2350a51f1a0
amadeu2018 <amadeualexandrealeg@gmail.com>
2025-07-15 21:50:58 +0000

    commit para fazer o pull das novas melhorias!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useRoutes } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { EventProvider } from "./contexts/EventContext";
@@ -22,6 +21,7 @@ import Sobre from "./pages/Sobre";
 import FirstOrder from "./pages/FirstOrder";
 import ResetPassword from "./pages/ResetPassword";
 import UpdatePassword from "./pages/UpdatePassword";
+import routes from "tempo-routes";
 
 const queryClient = new QueryClient();
 
@@ -34,6 +34,8 @@ const App = () => (
             <Toaster />
             <Sonner />
             <BrowserRouter>
+              {/* Tempo routes */}
+              {import.meta.env.VITE_TEMPO && useRoutes(routes)}
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/menu" element={<Menu />} />
@@ -50,6 +52,10 @@ const App = () => (
                 <Route path="/auth/update-password" element={<UpdatePassword />} />
                 <Route path="/perfil" element={<Profile />} />
                 <Route path="/sobre" element={<Sobre />} />
+                
+                {/* Add this before the catchall route */}
+                {import.meta.env.VITE_TEMPO && <Route path="/tempobook/*" />}
+                
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    // @ts-ignore
+    allowedHosts: process.env.TEMPO === "true" ? true : undefined
   },
   plugins: [
     react(),
+    tempo(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
@@ -18,5 +22,17 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  optimizeDeps: {
+    exclude: [
+      "tempo-devtools",
+      "cmdk",
+      "react-resizable-panels",
+      "lovable-tagger",
+    ],
+    include: [
+      "react-router-dom",
+      "lucide-react",
+    ],
   },
 }));
